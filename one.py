daa78c00049fc8ccc847284123d6c9f9bd7ca4a1
umugisha one <umugishaone@gmail.com>
2025-11-03 15:46:41 +0200

    Another change

--- one.py
+++ one.py
@@ -1,11 +1,3 @@
-number = int(input("Enter a number: "))
-
-print(f"You Entered the Number: {number}")
-
-print(f"Multiplication table of {number};")
-for i in range(1, 11):
-    print(f"{number} * {i} = {number * i}")
-
 try:
 
     number = int(input("Enter a number: "))
