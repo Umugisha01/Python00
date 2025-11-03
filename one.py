c16e3678ce5da5febf6f2d7e1225b64bda859ad3
umugisha one <umugishaone@gmail.com>
2025-11-03 15:10:49 +0200

    Adding exceptions

--- one.py
+++ one.py
@@ -1,7 +1,13 @@
-number = int(input("Enter a number: "))
 
-print(f"You Entered the Number: {number}")
+try:
 
-print(f"Multiplication table of {number};")
-for i in range(1, 11):
-    print(f"{number} * {i} = {number * i}")
+    number = int(input("Enter a number: "))
+
+    print(f"You Entered the Number: {number}")
+
+    print(f"Multiplication table of {number};")
+    for i in range(1, 11):
+        print(f"{number} * {i} = {number * i}")
+
+except ValueError:
+    print("Please enter a valid number!")
