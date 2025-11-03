25b73794b365de0d2591247f97bb2f138fa51c07
umugisha one <umugishaone@gmail.com>
2025-11-03 14:18:47 +0200

    feat: Add basic multiplication table script

---
+++ one.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number: "))
+
+print(f"You Entered the Number: {number}")
+
+print(f"Multiplication table of {number};")
+for i in range(1, 11):
+    print(f"{number} * {i} = {number * i}")
