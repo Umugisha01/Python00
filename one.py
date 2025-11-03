c4ceca3dfe03c15d20b366062f6086c5eb419992
umugisha one <umugishaone@gmail.com>
2025-11-03 15:04:26 +0200

    the current work

--- one.py
+++ one.py
@@ -1,1 +1,11 @@
+<<<<<<< HEAD
 
+=======
+number = int(input("Enter a number: "))
+
+print(f"You Entered the Number: {number}")
+
+print(f"Multiplication table of {number};")
+for i in range(1, 11):
+    print(f"{number} * {i} = {number * i}")
+>>>>>>> feature/Python00
